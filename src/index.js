fec349fdf9bc6eea8258bc4d58ea8530f8c8623e
nsalimova <nsalimova@oggettoweb.com>
2023-11-05 11:43:47 +0300

    Action Creator

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import { createStore } from 'redux';
 const reducer = (state = 0, action)=> {
     switch (action.type) {
+        case 'RND':
+            return state + action.payload;
+
         case 'INC':
             return ++state;
 
@@ -15,22 +18,27 @@ const reducer = (state = 0, action)=> {
 
 const store = createStore(reducer);
 
-document.
-    getElementById('inc').
-    addEventListener('click', ()=> {
-        store.dispatch({type: 'INC'})
+const inc = ()=> ({type: 'INC'});
+
+const dec = ()=> ({type: 'DEC'});
+
+const rnd = (payload)=> ({type: 'RND', payload});
+
+document.getElementById('inc').addEventListener('click', ()=> {
+    store.dispatch(inc());
 });
 
-document.
-    getElementById('inc').
-    addEventListener('click', ()=> {
-        store.dispatch({type: 'DEC'})
+document.getElementById('dec').addEventListener('click', ()=> {
+    store.dispatch(dec());
+});
+
+document.getElementById('rnd').addEventListener('click', ()=> {
+    const payload = + Math.floor(Math.random()*10);
+    store.dispatch(rnd(payload));
 });
 
 const update = ()=> {
-    document
-        .getElementById('counter')
-        .innerHTML = store.getState();
+    document.getElementById('counter').innerHTML = store.getState();
 }
 
 store.subscribe(update);
